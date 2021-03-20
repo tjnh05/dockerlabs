928e3c2de5da91ba99f569ef47ffeca053625e2c
tjnh05 <jyxz5@qq.com>
2021-03-20 13:06:23 +0800

    remove version limitatation

--- kubernetes/beginners/install/install-kube.sh
+++ kubernetes/beginners/install/install-kube.sh
@@ -1,20 +1,22 @@
 # Install Kubeadm, Kubelet, and Kubectl 
 #VERSION=1.18.6-00
-VERSION=1.20.0-00
+VERSION=1.20.5-00
 sudo apt-get update 
-sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION 
+#sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION 
+sudo apt-get install -y kubelet kubeadm kubectl 
 # Enable net.bridge.bridge-nf-call-iptables
 #echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf 
 
 # Temporarily disable IPv6 address
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
+#sysctl -w net.ipv6.conf.all.disable_ipv6=1
+#sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
 
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
